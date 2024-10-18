5b67b90f6d21c82df2cf33f8417cd247e1bf1a34
romero <hello@helloromero.co.uk>
2024-10-18 12:45:30 +0100

    adding UI

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -15,7 +15,7 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="">
+    <div className="min-w-full">
       {/* <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto w-100">
@@ -23,7 +23,16 @@ export default async function ProtectedPage() {
         </pre>
       </div> */}
 
-      <div className="relative rounded-lg border border-gray-100 gap-12">
+      <div className="flex justify-center items-center gap-12 ">
+        <span className="whitespace-nowrap rounded-full border border-purple-500 px-2 text-sm text-purple-700">
+          Live
+        </span>
+        <span className="whitespace-nowrap rounded-full border border-purple-500 px-2 text-sm text-purple-700">
+          Live
+        </span>
+      </div>
+
+      <div className="relative rounded-lg border border-gray-100 gap-12 mt-6">
         <label htmlFor="Search" className="sr-only">
           Search
         </label>
@@ -57,10 +66,10 @@ export default async function ProtectedPage() {
         </span>
       </div>
 
-      <div className="flex justify-center items-center flex-col gap-10">
+      <div className="flex justify-center items-center flex-col gap-10 mt-6">
         <a
           href="#"
-          className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8 w-1/2"
+          className="relative block overflow-hidden rounded-lg border border-gray-100 w-full p-6"
         >
           <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
 
@@ -83,50 +92,11 @@ export default async function ProtectedPage() {
           <div className="mt-4">
             <p className="text-pretty text-sm text-gray-500">
               Expo is an open-source platform for making universal native apps
-              for Android, iOS, and the web with JavaScript and React.
-            </p>
-          </div>
-
-          <dl className="mt-6 flex gap-4 sm:gap-6">
-            <div className="flex flex-col-reverse">
-              <dt className="text-sm font-medium text-gray-600"></dt>
-              <dd className="text-xs text-gray-500">31st June, 2021</dd>
-            </div>
-          </dl>
-          <dl className="mt-6 flex gap-4 sm:gap-6">
-            <div className="flex gap-4">
-              <dt className="text-sm font-medium text-gray-600">for_crony</dt>
-              <dt className="text-sm font-medium text-gray-600">tech</dt>
-              <dt className="text-sm font-medium text-gray-600">inspo</dt>
-            </div>
-          </dl>
-        </a>
-        <a
-          href="#"
-          className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8 w-1/2"
-        >
-          <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
-
-          <div className="sm:flex sm:justify-between sm:gap-4">
-            <div>
-              <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
-                Expo
-              </h3>
-            </div>
-
-            <div className="hidden sm:block sm:shrink-0">
-              <img
-                alt=""
-                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-                className="size-16 rounded-lg object-cover shadow-sm"
-              />
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <p className="text-pretty text-sm text-gray-500">
-              Expo is an open-source platform for making universal native apps
-              for Android, iOS, and the web with JavaScript and React.
+              for Android, iOS, and the web with JavaScript and React.Expo is an
+              open-source platform for making universal native apps for Android,
+              iOS, and the web with JavaScript and React.Expo is an open-source
+              platform for making universal native apps for Android, iOS, and
+              the web with JavaScript and React.
             </p>
           </div>
 
